5ef7b57299115e1b560f050bde0e14e07b1a4ca1
Sugiyanto Sutikno <sugiyanto.sutikno@gmail.com>
2025-02-19 07:08:12 +0700

    reset

--- models/host.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Host struct {
-	ID       string `json:"id"`
-	Name     string `json:"name"`
-	IP       string `json:"ip"`
-	OS       string `json:"os"`
-	Location string `json:"location"`
-}
